8ff4cd0fbdec49389b946e407a39fe5b0c563b81
Christoph Soehnel <csoehnel@gmail.com>
2019-03-04 08:49:31 +0100

    added script for downloading the prepared dataset

---
+++ datasets/bdd/download_nightdrive_dataset.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+fileid=""
+filename="20190303_bdd100k_sorted.tar.gz"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+tar -xvzf ${filename}
+rm ${filename}
