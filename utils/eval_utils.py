37e2d77823bac7475a5d71d0f1c5352f26f25a09
Christoph Soehnel <csoehnel@gmail.com>
2019-03-27 12:45:35 +0100

    class activation mapping for resnet18

--- utils/eval_utils.py
+++ utils/eval_utils.py
@@ -190,6 +190,77 @@ def get_filepaths(root, fnames_search, notfounderror=True):
     print(f"Found all files to be searched for in {root}.")
 
 
+# CAM code from https://github.com/metalbubble/CAM/blob/master/pytorch_CAM.py
+def get_CAM_resnet18(image_path, weights_path, class_dict, n_outputs, device):
+    import cv2
+    import torch
+    import torch.nn as nn
+    from PIL import Image
+    from torch.nn import functional as F
+    from torchvision import models, transforms
+    from torch.autograd import Variable
+
+    net = models.resnet18(pretrained = True)
+    finalconv_name = 'layer4'
+    net.fc = nn.Linear(net.fc.in_features, n_outputs)
+    net.load_state_dict(torch.load(weights_path)["model_state_dict"])
+    net.eval()
+
+    # hook the feature extractor
+    features_blobs = []
+
+    def hook_feature(module, input, output):
+        features_blobs.append(output.data.cpu().numpy())
+
+    net._modules.get(finalconv_name).register_forward_hook(hook_feature)
+
+    # get the softmax weight
+    params = list(net.parameters())
+    weight_softmax = np.squeeze(params[-2].data.numpy())
+
+    def returnCAM(feature_conv, weight_softmax, class_idx):
+        # generate the class activation maps upsample to 256x256
+        size_upsample = (256, 256)
+        bz, nc, h, w = feature_conv.shape
+        output_cam = []
+        for idx in class_idx:
+            cam = weight_softmax[idx].dot(feature_conv.reshape((nc, h * w)))
+            cam = cam.reshape(h, w)
+            cam = cam - np.min(cam)
+            cam_img = cam / np.max(cam)
+            cam_img = np.uint8(255 * cam_img)
+            output_cam.append(cv2.resize(cam_img, size_upsample))
+        return output_cam
+
+    normalize = transforms.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])
+    preprocess = transforms.Compose([transforms.Resize((224, 224)), transforms.ToTensor(), normalize])
+
+    img_pil = Image.open(image_path)
+    img_tensor = preprocess(img_pil)
+    img_variable = Variable(img_tensor.unsqueeze(0))
+
+    net.to(torch.device(device))
+    img_variable = img_variable.to(torch.device(device))
+    logit = net(img_variable)
+    logit = logit.cpu()
+
+    h_x = F.softmax(logit, dim = 1).data.squeeze()
+    probs, idx = h_x.sort(0, True)
+    probs = probs.numpy()
+    idx = idx.numpy()
+
+    # generate class activation mapping for the top1 prediction
+    CAMs = returnCAM(features_blobs[0], weight_softmax, [idx[0]])
+
+    # render the CAM and output
+    img = cv2.imread(image_path)
+    height, width, _ = img.shape
+    heatmap = cv2.applyColorMap(cv2.resize(CAMs[0], (width, height)), cv2.COLORMAP_JET)
+    result_bgr = heatmap * 0.3 + img * 0.5
+
+    return class_dict[idx[0]], probs[0], result_bgr
+
+
 if __name__ == '__main__':
     pass
 
